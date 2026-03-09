f9ffd36d936487ca6388094911bd09dda527e96b
holgertkey <holgertkey@gmail.com>
2026-03-09 09:17:40 +0200

    version command

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,12 @@ mod parser;
 mod repl;
 
 fn main() {
+    let args: Vec<String> = std::env::args().collect();
+
+    if args.len() > 1 && (args[1] == "-v" || args[1] == "--version") {
+        println!("ccalc v{}", env!("CARGO_PKG_VERSION"));
+        return;
+    }
+
     repl::run();
 }
